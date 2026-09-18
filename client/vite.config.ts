b51e3671769ea33da838853f3271f25c9fdde115
Lee <lee.samchong@gmail.com>
2026-09-18 17:12:07 -0400

    Refactored vite configs for pwa support

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,43 @@
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import {VitePWA} from 'vite-plugin-pwa'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+
+    VitePWA({
+      registerType: 'autoUpdate',
+
+      manifest: {
+        name: 'Joblet',
+        short_name: 'joblet',
+        description: 'Job recruitment site',
+        theme_color: '#00008B',
+        background_color: '#f3f4f6',
+        display: 'standalone',
+
+        icons: [
+          {
+            src: '/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      },
+
+      workbox: {
+        globPatterns: [
+          '**/*.{js,css,html,ico,png,svg}'
+        ]
+      }
+    })
+  ]
 })
