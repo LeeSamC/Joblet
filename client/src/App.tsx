9969ae0050adc2982baf86801c365b85780e283b
Lee <lee.samchong@gmail.com>
2026-09-21 18:13:40 -0400

    Added an auth initialization to help with flow

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,10 +2,12 @@ import {RouterProvider} from 'react-router-dom'
 
 import {router} from './app/router'
 import {AppProviders} from './app/providers';
+import AuthInitializer from './app/AuthInitializer';
 
 export default function App(){
   return (
     <AppProviders>
+      <AuthInitializer />
       <RouterProvider router={router} />
     </AppProviders>
   )
