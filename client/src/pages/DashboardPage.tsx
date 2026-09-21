49aa0d229b349bc7f45f04961373039745ed4176
Lee <lee.samchong@gmail.com>
2026-09-21 18:15:00 -0400

    testing dashboard page

--- client/src/pages/DashboardPage.tsx
+++ client/src/pages/DashboardPage.tsx
@@ -3,5 +3,15 @@ import { useState } from "react";
 import { useAuthStore } from "../stores/auth.store";
 
 export default function DashboardPage(){
-    
+
+    const user = useAuthStore(state => state.user)
+
+
+    return(
+        <div className="flex items-center justify-center text-2xl">
+            <h2>
+               Welcome {user?.firstName} 
+            </h2>
+        </div>
+    )
 }
